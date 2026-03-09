8856af7e90398bce9d7336f9fc4f31d3d54b01d6
Rafael <rafael.bzonta@gmail.com>
2026-03-09 16:47:45 -0300

    Create loPTrRl001.sql

---
+++ loPTrRl001.sql
@@ -0,0 +1,33 @@
+SELECT
+
+    -- ativo
+    entity_name AS 'Conta',
+    make AS 'Fabricante',
+    model AS 'Modelo',
+    serial_number AS 'Número de Série',
+    asset_id AS 'ID do Ativo',
+    
+    -- rede
+    ip_address AS 'Endereço IP',
+    mac_address AS 'Mac Address',
+
+    ROUND(JULIANDAY(last_meter_timestamp) - JULIANDAY(first_meter_timestamp), 2) AS 'Dias entre Medidores',
+
+    DATETIME(first_meter_timestamp) AS 'Data Primeiro Medidor',
+    DATETIME(last_meter_timestamp) AS 'Data Último Medidor',
+    -- coleta ultimo medidor
+    last_pageCounts_life_a3 AS 'Medidor A3',
+    
+    -- obs: se o medidor foi resetado ou a placa trocada, este valor pode vir negativo
+    (last_pageCounts_life_a3 - first_pageCounts_life_a3) AS 'Volume Total A3' 
+
+FROM meters
+
+WHERE 
+    managed = true 
+    AND model LIKE '%WF-C878R%'
+
+ORDER BY 
+    entity_name ASC, 
+    make ASC, 
+    model ASC;
